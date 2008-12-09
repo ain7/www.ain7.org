2f8ac6de60f2e780174c0ada7256e15d2f762b3f
Laurent Bives <laurent07@gmail.com>
2008-12-09 23:44:43 +0100

    /*  * Modifications mineures du thème Ain7 de la galerie.  */ Mise à jour des urls du portail utilisées par la galerie,   . ain7_path = 'http://www.dev.ain7.info/';   . ain7_forum = 'http://forum.dev.ain7.info/';   . ain7_galerie = 'http://galerie.dev.ain7.info';   . ain7_planet = 'http://planet.ain7.info/';
    Suppression des informations EXIF pour les photographies en affichage plein écran.
      . pas de suppression de l'information, information seulement non affichée via CSS
    
    Modification des INSTALL des thèmes Ain7 forum et galerie,
      . modifier les urls du portail dans le fichier de conf.

--- ain7-website/galerie/themes/ain7/settings/ain7_config.php
+++ ain7-website/galerie/themes/ain7/settings/ain7_config.php
@@ -29,9 +29,9 @@
 	define('AIN7_CONFIG_LOADED', 1);
 
 	$ain7_path = 'http://www.dev.ain7.info/';
-	$ain7_forum = 'http://ain7-v12.laula.homelinux.org/';
-	$ain7_galerie = 'http://ain7.galerie.laula.homelinux.org';
+	$ain7_forum = 'http://forum.dev.ain7.info/';
+	$ain7_galerie = 'http://galerie.dev.ain7.info';
 	$ain7_planet = 'http://planet.ain7.info/';
-	$ain7_version = '0.0.0';
+	$ain7_version = '0.0.1';
 
 ?>
