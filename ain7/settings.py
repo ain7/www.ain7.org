79edcf52dc615497991cd3be0aa71c66ec48413b
Laurent Bives <laurent@bives.org>
2011-09-28 21:51:40 +0200

    Fix lp #861743

--- ain7/settings.py
+++ ain7/settings.py
@@ -194,6 +194,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
     'django.core.context_processors.static',
+    'django.core.context_processors.request',
     'django.contrib.messages.context_processors.messages',
     'ain7.context_processors.versions',
     'ain7.context_processors.piwik',
