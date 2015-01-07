3d148dfa7ddd5c69212a1f003c488c77379a1596
Lionel Porcheron <lionel@alveonet.org>
2015-01-07 21:50:30 +0100

    Restructure settings.py: add granularity, removed unused stuff

--- ain7/settings.py
+++ ain7/settings.py
@@ -27,10 +27,6 @@ import django.conf.global_settings as DEFAULT_SETTINGS
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '45@qqvbavfu*3)@e6vs#z8tp0_l^q^)aota0)2_lw^fh(96rim'
 
@@ -41,35 +37,21 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['www.ain7.com', 'ain7.com', 'www.ain7.org', 'ain7.org']
 
-
 # Application definition
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
-    'autocomplete_light',
-    'crispy_forms',
-    'endless_pagination',
     'grappelli',
-    'ain7.adhesions',
-    'ain7.annuaire',
-    'ain7.association',
-    'ain7.emploi',
-    'ain7.groups',
-    'ain7.manage',
-    'ain7.news',
-    'ain7.organizations',
-    'ain7.pages',
-    'ain7.shop',
-    'ain7.voyages',
 )
 
 MIDDLEWARE_CLASSES = (
+    'ain7.middleware.forcelocale.ForceDefaultLanguageMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -77,16 +59,11 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'ain7.middleware.forcelocale.ForceDefaultLanguageMiddleware',
-#    'ain7.middleware.useractivity.UserActivityMiddleware',
-    'ain7.middleware.forcelocale.ForceDefaultLanguageMiddleware',
-    'ain7.middleware.portalexceptions.PortalException',
 )
 
 ROOT_URLCONF = 'ain7.urls'
 
 WSGI_APPLICATION = 'ain7.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
@@ -102,46 +79,36 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'fr'
-
 TIME_ZONE = 'Europe/Paris'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
-
 MEDIA_URL = '/site_media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'ain7', 'media')
 
 #
 # Added for this project
 #
 
 TEMPLATE_DIRS = DEFAULT_SETTINGS.TEMPLATE_DIRS + (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Put strings here, like "/home/html/django_templates"
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     BASE_DIR + '/ain7/templates',
 )
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'ain7', 'static'),
-)
-
 TEMPLATE_CONTEXT_PROCESSORS = DEFAULT_SETTINGS.TEMPLATE_CONTEXT_PROCESSORS + (
     'django.core.context_processors.request',
+    'django.contrib.auth.context_processors.auth',
+    'allauth.account.context_processors.account',
+    'allauth.socialaccount.context_processors.socialaccount',
     'ain7.context_processors.piwik',
     'ain7.context_processors.user_groups',
 )
 
-PIWIK_ENABLED = False
 
 DEFAULT_FROM_EMAIL = 'AIn7 <noreply@ain7.com>'
 
@@ -154,17 +121,78 @@ EMAIL_PORT = 1025
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 
+SITE_ID = 1
 
+# autocomplete_light
+INSTALLED_APPS += (
+    'autocomplete_light',
+)
+
+# celery configuration
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_IMPORTS = ("ain7.tasks",)
+
+# endless configuration
+INSTALLED_APPS += (
+    'endless_pagination',
+)
 ENDLESS_PAGINATION_PER_PAGE = 25
 
+# django-crispy-forms
+INSTALLED_APPS += (
+    'crispy_forms',
+)
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+# auth and allauth settings
+INSTALLED_APPS += (
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.facebook',
+)
+AUTHENTICATION_BACKENDS = DEFAULT_SETTINGS.AUTHENTICATION_BACKENDS + (
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+LOGIN_REDIRECT_URL = '/'
+SOCIALACCOUNT_QUERY_EMAIL = True
+
+# AIn7 specific stuff
+
+INSTALLED_APPS += (
+    'ain7',
+    'ain7.adhesions',
+    'ain7.annuaire',
+    'ain7.association',
+    'ain7.emploi',
+    'ain7.groups',
+    'ain7.manage',
+    'ain7.news',
+    'ain7.organizations',
+    'ain7.pages',
+    'ain7.shop',
+    'ain7.voyages',
+)
+
+MIDDLEWARE_CLASSES += (
+    'ain7.middleware.useractivity.UserActivityMiddleware',
+    'ain7.middleware.forcelocale.ForceDefaultLanguageMiddleware',
+    'ain7.middleware.portalexceptions.PortalException',
+)
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'ain7', 'static'),
+)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'ain7', 'media')
+
+PIWIK_ENABLED = False
 PIWIK_URL = 'http://localhost/piwik/'
 PIWIK_SITE_ID = '0'
 
 PORTAL_ADMIN = 'ain7-admin'
 AIN7_MEMBERS = 'ain7-membre'
 AIN7_CONTRIBUTORS = 'ain7-contributor'
-
-CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 FACEBOOK_AIN7 = 'http://www.facebook.com/ENSEEIHT'
 LINKEDIN_AIN7 = 'http://www.linkedin.com/groups?gid=73525'
@@ -177,9 +205,6 @@ SYSTEM_PAY_CERTIFICATE = '1234567890912345'
 SYSTEM_PAY_MODE = 'TEST'
 
 AIN7_SIRET = '0000000000000001-001'
-
-BROKER_URL = 'redis://localhost:6379/0'
-CELERY_IMPORTS = ("ain7.tasks",)
 
 try:
     from settings_local import *
