15374b8663e4b4180263b2a5c6a95d8e495ed01a
François Magimel <francois.magimel@alumni.enseeiht.fr>
2016-05-11 21:05:40 +0200

    Remove useless imports in the main urls file

--- ain7/urls.py
+++ ain7/urls.py
@@ -21,9 +21,7 @@
 #
 
 from django.conf.urls import include, url
-from django.conf.urls.static import static
 from django.conf import settings
-from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.contrib.sitemaps import views as sitemaps_views
 from django.views.generic.base import RedirectView
