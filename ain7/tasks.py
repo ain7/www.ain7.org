e39113af8306629f99b33cd7bbc11bf132952076
Lionel Porcheron <lionel@alveonet.org>
2017-10-29 20:09:45 +0100

    Add new tasks to update subscription status and notify of notifications

--- ain7/tasks.py
+++ ain7/tasks.py
@@ -20,14 +20,19 @@ from __future__ import absolute_import
 
 import datetime
 import logging
+import os
 
 from celery import shared_task
 
 from django.conf import settings
+from django.core.mail import EmailMessage, EmailMultiAlternatives
 from django.db.models import Q
+from django.template.loader import render_to_string
 from django.utils import timezone
 
-from ain7.adhesions.models import Subscription
+from email.mime.image import MIMEImage
+
+from ain7.adhesions.models import Subscription, SubscriptionKey
 from ain7.annuaire.models import AIn7Member
 from ain7.emploi.models import JobOffer
 from ain7.groups.models import Group, Member
@@ -67,7 +72,7 @@ def refresh_membership():
     Member.objects.filter(group=members).delete()
 
     for member in AIn7Member.objects.all():
-        if member.is_subscriber():
+        if member.person.is_subscriber:
             members.add(member.person)
 
 
@@ -82,10 +87,74 @@ def notify_expiring_membership():
     subscriptions = Subscription.objects.filter(
         Q(end_date=in_thirty_days) | Q(end_date=in_seven_days) |
         Q(end_date=today) | Q(end_date=twenty_days_ago)
-    ).exclude(member__person__emails__isnull=True)
+    ).distinct().exclude(member__person__emails__isnull=True).exclude(validated=False)
     for sub in subscriptions:
         if not sub.member.has_subscription_next():
             try:
                 sub.member.notify_expiring_membership()
             except:
                 pass
+
+@shared_task
+def notify_staff_membership_operations():
+
+    import locale
+    try:
+        locale.setlocale(locale.LC_TIME,'fr_FR.UTF-8')
+    except Exception:
+        pass
+
+    now = timezone.now()
+    seven_days_ago = now - datetime.timedelta(days=7)
+
+    notified_members = SubscriptionKey.objects.filter(created_at__gte=seven_days_ago).distinct()
+    expired_members = Subscription.objects.filter(validated=True, end_date__gte=seven_days_ago, end_date__lte=now.date()).distinct()
+    renew_members = Subscription.objects.filter(validated=True, date__gte=seven_days_ago.date()).distinct()
+
+    subject = u'Adhésions AIn7: activité de la semaine du '+seven_days_ago.strftime("%d %B %Y")
+    html_content = render_to_string(
+        'emails/notification_staff_subscriptions_operations.html', {
+        'notified_members': notified_members,
+        'expired_members': expired_members,
+        'renew_members': renew_members,
+        'settings': settings,
+    })
+
+    text_content = render_to_string(
+        'emails/notification_staff_subscriptions_operations.txt', {
+        'notified_members': notified_members,
+        'expired_members': expired_members,
+        'renew_members': renew_members,
+        'settings': settings,
+    })
+    msg = EmailMultiAlternatives(
+        subject,
+        text_content,
+        'adhesions@ain7.com',
+        ['adhesions@ain7.com'],
+    )
+
+    msg.mixed_subtype = 'related'
+
+    for img in ['logo_ain7.png', 'facebook.png', 'linkedin.png', 'twitter.png', 'googleplus.png']:
+        fp = open(os.path.join(os.path.dirname(__file__), 'templates/emails/img', img), 'rb')
+        msg_img = MIMEImage(fp.read())
+        fp.close()
+        msg_img.add_header('Content-ID', '<{}>'.format(img))
+        msg.attach(msg_img)
+
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
+
+@shared_task
+def update_subscription_flag():
+
+    from ain7.annuaire.models import Person
+
+    for p in Person.objects.all():
+        try:
+            p.is_subscriber = p.ain7member.is_subscriber()
+            p.save()
+        except Exception:
+            pass
+
